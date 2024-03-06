5340e03b3f362cad4f45639bc8818dc860c4b7d1
rorrxr <rorrxr@naver.com>
2024-03-06 15:44:40 +0900

    first commit

---
+++ svt/src/main/java/com/foodmap/SvtApplication.java
@@ -0,0 +1,13 @@
+package com.foodmap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SvtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SvtApplication.class, args);
+	}
+
+}
