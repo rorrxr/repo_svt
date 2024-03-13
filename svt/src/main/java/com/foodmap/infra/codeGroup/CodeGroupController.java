c76b385b7f3a0a0dc8ed9e41f8fb60358d1a43a4
rorrxr <rorrxr@naver.com>
2024-03-13 14:59:41 +0900

    update datepicker select and validation

--- svt/src/main/java/com/foodmap/infra/codeGroup/CodeGroupController.java
+++ svt/src/main/java/com/foodmap/infra/codeGroup/CodeGroupController.java
@@ -6,6 +6,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.foodmap.constants.Constants;
+import com.foodmap.util.UtilDateTime;
+
 @Controller
 public class CodeGroupController {
 	
@@ -26,7 +29,28 @@ public class CodeGroupController {
 //
 //		return "xdm/v1/infra/ddm/tables";
 //	}
-
+	public void setSearch(CodeGroupVo vo) throws Exception {
+		/* 최초 화면 로딩시에 세팅은 문제가 없지만 */
+		/*이후 전체적으로 데이터를 조회를 하려면 null 값이 넘어 오는 관계로 문제가 전체 데이터 조회가 되지 못한다.*/
+		/*해서 BaseVo.java 에서 기본값을 주어서 처리*/
+//		vo.setShUseNy(vo.getShUseNy() == null ? 1 : vo.getShUseNy());
+//		vo.setShDelNy(vo.getShDelNy() == null ? 0 : vo.getShDelNy());
+//		vo.setShOptionDate(vo.getShOptionDate() == null ? 2 : vo.getShOptionDate());
+		
+		/* 초기값 세팅이 있는 경우 사용 */
+		vo.setShDateStart(vo.getShDateStart() == null
+		    ? UtilDateTime.calculateDayReplace00TimeString(UtilDateTime.nowLocalDateTime(), Constants.DATE_INTERVAL)
+		    : UtilDateTime.add00TimeString(vo.getShDateStart()));
+		vo.setShDateEnd(vo.getShDateEnd() == null
+		    ? UtilDateTime.nowString()
+		    : UtilDateTime.addNowTimeString(vo.getShDateEnd()));		
+		
+//		/* 초기값 세팅이 없는 경우 사용 */
+//		vo.setShDateStart(vo.getShDateStart() == null || vo.getShDateStart() == "" ? null : UtilDateTime.add00TimeString(vo.getShDateStart()));
+//		vo.setShDateEnd(vo.getShDateEnd() == null || vo.getShDateEnd() == "" ? null : UtilDateTime.add59TimeString(vo.getShDateEnd()));
+		
+		
+	}
 	@RequestMapping(value = "/codeGroupView")
 	public String codeGroupView(CodeGroupDto dto, Model model) throws Exception{
 //		System.out.println("dto.getSeq() : " + dto.getSeq());
@@ -101,6 +125,8 @@ public class CodeGroupController {
 		System.out.println("vo.getShDateStart() : " + vo.getShDateStart());
 		System.out.println("vo.getShDateEnd() : " + vo.getShDateEnd());
 		System.out.println("=======");
+		
+		setSearch(vo);
 		
 		model.addAttribute("list", service.selectList(vo));
 
